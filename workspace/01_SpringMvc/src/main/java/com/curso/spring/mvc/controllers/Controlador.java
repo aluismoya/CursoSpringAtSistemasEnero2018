31198d07b07468d018414df940661af8f4a15d73
emartin <emartin>
2018-01-25 14:56:28 +0100

    Dia 4

--- workspace/01_SpringMvc/src/main/java/com/curso/spring/mvc/controllers/Controlador.java
+++ workspace/01_SpringMvc/src/main/java/com/curso/spring/mvc/controllers/Controlador.java
@@ -4,9 +4,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,9 +65,13 @@ public class Controlador {
 	 * @return
 	 */
 	@RequestMapping(path="/cambioDeNombre", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_XML_VALUE)
-	@ResponseBody public Persona cambiarElNombreALaPersona(@RequestBody Persona persona, @RequestParam(required=true) String nombreNuevo) {
+	@ResponseBody public ResponseEntity<Persona> cambiarElNombreALaPersona(@RequestBody Persona persona, @RequestParam(required=true) String nombreNuevo) {
 		persona.setNombre(nombreNuevo);
-		return persona;
+		HttpHeaders headers = new HttpHeaders();
+		
+		headers.setLocation(null);
+		
+		return new ResponseEntity<Persona>(persona, headers , HttpStatus.CREATED);
 	}
 	
 }
---
+++ workspace/01_SpringMvc/src/main/java/com/curso/spring/mvc/persistence/PersonaRepositoryImpl.java
@@ -0,0 +1,19 @@
+package com.curso.spring.mvc.persistence;
+
+import org.springframework.stereotype.Repository;
+
+import com.curso.spring.mvc.entities.Persona;
+
+@Repository
+public class PersonaRepositoryImpl implements PersonaRepository {
+
+	/* (non-Javadoc)
+	 * @see com.curso.spring.mvc.persistence.PersonaRepository#save(com.curso.spring.mvc.entities.Persona)
+	 */
+	@Override
+	public void save(Persona unMarshall) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
